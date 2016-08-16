71769612a9c4f02533745b7e3944987cff283cb5
joeybloggs <Dean.Karn@gmail.com>
2016-08-16 11:27:28 -0400

    add ability to override host ip

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [ "$KEY_PATH" -a "$CERTIFICATE_PATH" ]; then
 fi
 
 opts=(
-	dc_local_interfaces "[0.0.0.0]:${PORT:-25} ; [::0]:${PORT:-25}"
+	dc_local_interfaces "[${HOST_IP:-0.0.0.0}]:${PORT:-25} ; [::0]:${PORT:-25}"
 	dc_other_hostnames ''
 	dc_relay_nets "$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')${RELAY_NETWORKS}"
 )
